47f7f5e499b981cfa8d74af388e5e79cccb8f7c2
Fauzan Ahmad <fauzanahmadgojeck@gmail.com>
2021-09-24 22:53:37 +0700

    update logo

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -35,7 +35,7 @@ export default function CartItem({item, value}) {
                 </div>
             </div>
             <div className="col-10 mx-auto col-lg-2">
-                <div className="cart-icon"  onClick={()=>removeItem(id)}>
+                <div className="text-danger"  onClick={()=>removeItem(id)}>
                     <div className="fas fa-trash"></div>
                 </div>
             </div>
